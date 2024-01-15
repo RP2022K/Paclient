6fd7212f62bb928b98eed6e0b93b6c8660fa6912
RP <rp@example.com>
2024-01-15 09:08:35 +0100

    first

---
+++ src/TodoService.java
@@ -0,0 +1,17 @@
+
+import hu.szit.Client;
+
+public class TodoService {
+    String url;
+    Client client;
+
+    public TodoService()
+    {
+        url = "https://jsonplaceholder.typicode.com/todos";
+        client = new Client();
+    }
+
+    public String GetTodos(){
+        return client.get(url);
+    }
+}
